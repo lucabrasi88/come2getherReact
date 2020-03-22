2a96b3eb7d3fb9b6fa7a438fb61c7e665700fb58
lukaszczaczyk <lukaszczaczyk@hotmail.com>
2020-03-22 21:38:59 +0100

    Calendar view elements

---
+++ src/Calendar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function ListEvents(existingEvents){
+
+}
+
+function NewEvent(newEvent){
+
+}
+
+function CalendarItem(){
+    
+}
---
+++ src/DbHandler.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CreateButton(){
+    var buttonElement = <button className ="createButton"> Create event</button>;
+    return buttonElement;
+}
+
+export default DbHandler;

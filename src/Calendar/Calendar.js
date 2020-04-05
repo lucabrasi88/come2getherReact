3985fc6d57f218dc4a91875f72f5d756269a1dd1
lukaszczaczyk <lukaszczaczyk@hotmail.com>
2020-04-05 10:59:04 +0200

    Calendar done, styles

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -256,14 +256,14 @@ export default class Calendar extends React.Component {
               onClick={e => {
                 this.onPrev();
               }}
-              class="calendar-button button-prev"
+              className="calendar-button button-prev"
             />
             {!this.state.showMonthTable && (
               <span
                 onClick={e => {
                   this.showMonth();
                 }}
-                class="calendar-label"
+                className="calendar-label"
               >
                 {this.month()}
               </span>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Calendar from "./Calendar/Calendar";
+import EventLines from "./Events/EventLine";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="row">
+      <div className="col-sm-6">
       <Calendar />
+      </div>     
+      <div className="col-sm-6">
+        <EventLines key="0" date="poniedziałek" name="Halfmarathon Poznań"/>
+        <EventLines key="1" date="poniedziałek" name="Halfmarathon Poznań"/>
+        <EventLines key="2" date="poniedziałek" name="Halfmarathon Poznań"/>
+        <EventLines key="3" date="poniedziałek" name="Halfmarathon Poznań"/>
+      </div>
     </div>
   );
 }
